c5448b68041078ce78e5166ceedec64e785d5f3b
DavidB360 <david.bonnet360@gmail.com>
2022-12-12 12:01:21 +0100

    setup backend

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+require('./models/connection');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var tutorialsRouter = require('./routes/tutorials');
+var helprequestsRouter = require('./routes/helprequests');
+
+var app = express();
+
+const cors = require('cors');
+app.use(cors());
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/tutorials', tutorialsRouter);
+app.use('/helprequests', helprequestsRouter)
+
+module.exports = app;
---
+++ modules/checkBody.js
@@ -0,0 +1,14 @@
+function checkBody(body, keys) {
+    let isValid = true;
+  
+    for (const field of keys) {
+      if (!body[field] || body[field] === '') {
+        isValid = false;
+      }
+    }
+  
+    return isValid;
+  }
+  
+  module.exports = { checkBody };
+  
