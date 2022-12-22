7e661e9535d7ae89579ae0882c0da9016f2a2187
DavidB360 <david.bonnet360@gmail.com>
2022-12-22 17:16:16 +0100

    commit suite à modification route uploadPic dans route index

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,16 @@ const cloudinary = require('cloudinary').v2;
 const fs = require('fs');
 const uniqid = require('uniqid');
 
-router.post('/upload', async (req, res) => {
+router.post('/uploadPic', async (req, res) => {
 
     // sauvegarde temporaire de la photo sur le backend récupérée via la props photoFromFront à définir côté frontend à l'envoi du POST
-    // on génère un nom unique des fois que plusieurs utilisateurs utilisent en même temps et écrase la photo avant qu'elle ait été envoyée
+    // on génère un nom unique des fois que plusieurs utilisateurs utilisent en même temps l'app et écrasent la photo avant qu'elle n'ait été envoyée
     const photoPath = `./tmp/${uniqid()}.jpg`;
     const resultMove = await req.files.photoFromFront.mv(photoPath); 
     
     if(!resultMove) {
         // si la copie s'est bien passée, on envoie la photo sur cloudinary et on efface la photo temporaire avec fs
-        const resultCloudinary = await cloudinary.uploader.upload(photoPath);
+        const resultCloudinary = await cloudinary.uploader.upload(photoPath, {folder : "lemulot/"});
         fs.unlinkSync(photoPath);
         res.json({ result: true, url: resultCloudinary.secure_url });
     } else {
