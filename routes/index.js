2bc3d9a1a4ebd9abbc8b8996e9e255432922358d
DavidB360 <david.bonnet360@gmail.com>
2022-12-22 01:00:08 +0100

    ajout route cloudinary dans index

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const cloudinary = require('cloudinary').v2;
+const fs = require('fs');
+const uniqid = require('uniqid');
 
+router.post('/upload', async (req, res) => {
+
+    // sauvegarde temporaire de la photo sur le backend récupérée via la props photoFromFront à définir côté frontend à l'envoi du POST
+    // on génère un nom unique des fois que plusieurs utilisateurs utilisent en même temps et écrase la photo avant qu'elle ait été envoyée
+    const photoPath = `./tmp/${uniqid()}.jpg`;
+    const resultMove = await req.files.photoFromFront.mv(photoPath); 
+    
+    if(!resultMove) {
+        // si la copie s'est bien passée, on envoie la photo sur cloudinary et on efface la photo temporaire avec fs
+        const resultCloudinary = await cloudinary.uploader.upload(photoPath);
+        fs.unlinkSync(photoPath);
+        res.json({ result: true, url: resultCloudinary.secure_url });
+    } else {
+        res.json({ result: false, error: resultCopy });
+    }
+});
+//
 
 module.exports = router;
