9f21ff036e83a2a5313786489e83018c00265906
DavidB360 <david.bonnet360@gmail.com>
2023-01-08 12:20:09 +0100

    ajout mongoose.set('strictQuery', true) dans le modèle connection.js pour supprimer le warning Mongoose et se préparer à la mise à jour 7 de Mongoose. Paramétrer strictQuery à true assure que seules les données conformes au schéma seront enregistrées dans la base de données, c'était le réglage par défaut mais il va évoluer à false dans la future version, d'où le besoin d'ajouter une ligne pour le forcer à true si c'est notre souhait

--- models/connection.js
+++ models/connection.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const connectionString = process.env.CONNECTION_STRING;
 
+mongoose.set('strictQuery', true);
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
   .catch(error => console.error(error));
